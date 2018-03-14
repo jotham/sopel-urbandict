993f4d2592a5b2e13b9331914a358969e7dfa3ea
Jotham Read <jotham.read@gmail.com>
2018-03-15 11:27:09 +1300

    Tidy up, added multiline results

--- urbandict.py
+++ urbandict.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import requests
 
 BASE_URL = "http://api.urbandictionary.com/v0/define?term={}"
-
+MAX_LINES = 2 # Maximum return lines for top definition
 
 def urban_search(term):
     query = requests.get(BASE_URL.format(term)).json()
@@ -13,25 +13,23 @@ def urban_search(term):
         except (IndexError, KeyError) as error:
             return None
 
-
 try:
     import sopel.module
 except ImportError:
     pass
 else:
     from sopel.formatting import underline, bold
-
     @sopel.module.commands('urb', 'urban')
     @sopel.module.example('.urb cunt')
     def f_urban(bot, trigger):
         """Search Urban Dictionary for a dank definition"""
-        query = trigger.group(2).strip().lower()
-        results = urban_search(query)
-        if results:
-            bot.say('{} — {}'.format(underline(results[0]), results[1]))
+        if trigger.group(2):
+           query = trigger.group(2).strip().lower()
+           results = urban_search(query)
+           if results:
+               bot.say('{} — {}'.format(underline(results[0]), results[1]), trigger.sender, MAX_LINES)
         else:
             bot.say('Couldn\'t find anything for {}'.format(bold(query)))
-
 
 if __name__ == '__main__':
     import sys
